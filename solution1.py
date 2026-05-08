e3b0416198c072f0cb0446cb89ddd77154799aee
Pål Bergerskogen <pal.bergerskogen@gmail.com>
2026-05-08 12:45:54 +0200

    refactor: extract FILE_PATH and rename solution 1 file

--- solution1.py
+++ solution1.py
@@ -1,7 +1,6 @@
+from constants.FILE_PATH import FILE_PATH
 from utils.print_call_statistics import print_call_statistics
 from utils.read_call_records_from_file import read_call_records_from_file
-
-FILE_PATH = "assets/crds.json"
 
 if __name__ == "__main__":
     callRecords = read_call_records_from_file(FILE_PATH)
